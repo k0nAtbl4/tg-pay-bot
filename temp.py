f13561b4fcaf7a0616a683f8a3e8f76332eea65b
kostyan <gsbeshsvzb620@gmail.com>
2025-01-21 08:20:30 +0300

    delete .env ignoring

--- temp.py
+++ temp.py
@@ -1,6 +1,12 @@
-from datetime import datetime
-
-
-a=1737395666
-last_time = (datetime.fromtimestamp(a).strftime('%Y-%m-%d %H:%M:%S'))
-print(last_time)
+for n in range(1, 100):
+    s = bin(n)[2:]  # перевод в двоичную систему
+    s = str(s)
+    s = s[s.find('1'):]
+    if s.count('1') > s.count('0'):
+        s += '1'
+    else:
+        s += '0'
+    r = int(s, 2)  # перевод в десятичную систему
+    if r > 80:
+        print(r)
+        break
